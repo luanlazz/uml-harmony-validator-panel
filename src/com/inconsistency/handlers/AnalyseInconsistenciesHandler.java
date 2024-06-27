138dabbf1a7c3340522db8b4621812cce1ad3fb9
luanlazz <luanlazzari@hotmail.com>
2024-06-27 01:15:29 -0300

    feat: eclipse plugin for a dashboard of uml inconsistencies

---
+++ src/com/inconsistency/handlers/AnalyseInconsistenciesHandler.java
@@ -0,0 +1,79 @@
+package com.inconsistency.handlers;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+
+import com.inconsistency.InconsistencyPanel;
+import com.inconsistency.dto.AnalyserResponseDTO;
+import com.inconsistency.dto.InconsistencyErrorDTO;
+import com.inconsistency.services.InconsistencyAnalyserAPI;
+
+public class AnalyseInconsistenciesHandler extends AbstractHandler {
+
+	private InconsistencyAnalyserAPI analyserService = new InconsistencyAnalyserAPI();
+
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) {
+		List<InconsistencyErrorDTO> inconsistencies = new ArrayList();
+
+		try {
+			AnalyserResponseDTO analyseResponse = analyseActiveEditor();
+
+			if (analyseResponse.getSuccess()) {
+				Thread.sleep(1000);
+
+				inconsistencies = fecthInconsistenciesByClientId(analyseResponse.getClientId());
+
+				InconsistencyPanel.instace().updateViewData(inconsistencies);
+			}
+		} catch (Exception e) {
+			System.out.println("Error analyse model: " + e.getMessage());
+		}
+
+		return inconsistencies;
+	}
+
+	private AnalyserResponseDTO analyseActiveEditor() throws Exception {
+		IWorkbenchPart workbenchPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+				.getActivePart();
+
+		IEditorPart activeEditor = workbenchPart.getSite().getPage().getActiveEditor();
+		if (activeEditor == null) {
+			throw new ExecutionException("Open a file first!");
+		}
+
+		IFile file = activeEditor.getEditorInput().getAdapter(IFile.class);
+		if (file == null) {
+			throw new ExecutionException((new FileNotFoundException()).getMessage());
+		}
+
+		AnalyserResponseDTO analyseResponse = analyserService.analyseFile(file);
+		if (!analyseResponse.getSuccess()) {
+			throw new ExecutionException(analyseResponse.getError());
+		}
+
+		return analyseResponse;
+	}
+
+	private List<InconsistencyErrorDTO> fecthInconsistenciesByClientId(String clientId) throws Exception {
+		List<InconsistencyErrorDTO> response = analyserService.getInconsistenciesByClientId(clientId);
+
+		return response;
+	}
+}
---
+++ src/com/inconsistency/services/InconsistencyAnalyserAPI.java
@@ -0,0 +1,134 @@
+package com.inconsistency.services;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+
+import com.inconsistency.dto.AnalyserResponseDTO;
+import com.inconsistency.dto.InconsistencyErrorDTO;
+import com.inconsistency.utils.Json2Obj;
+
+public class InconsistencyAnalyserAPI {
+
+	private final String urlBase = "http://localhost:8080/kafka";
+
+	public AnalyserResponseDTO analyseFile(IFile iFile) {
+		String url = urlBase + "/send";
+		String charset = "UTF-8";
+		String param = "/send";
+		File textFile = iFile.getRawLocation().makeAbsolute().toFile();
+		String boundary = Long.toHexString(System.currentTimeMillis()); // Just generate some unique random value.
+		String CRLF = "\r\n"; // Line separator required by multipart/form-data.
+		HttpURLConnection connection = null;
+
+		try {
+			connection = (HttpURLConnection) new URL(url).openConnection();
+			connection.setDoOutput(true);
+			connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+			OutputStream output = connection.getOutputStream();
+			PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
+
+			// Send normal param.
+			writer.append("--" + boundary).append(CRLF);
+			writer.append("Content-Disposition: form-data; name=\"param\"").append(CRLF);
+			writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
+			writer.append(CRLF).append(param).append(CRLF).flush();
+
+			// Send text file.
+			writer.append("--" + boundary).append(CRLF);
+			writer.append("Content-Disposition: form-data; name=\"file\"; filename=\"" + textFile.getName() + "\"")
+					.append(CRLF);
+			writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF); // Text file itself must be
+																						// saved in this charset!
+			writer.append(CRLF).flush();
+			Files.copy(textFile.toPath(), output);
+			output.flush(); // Important before continuing with writer!
+			writer.append(CRLF).flush(); // CRLF is important! It indicates end of boundary.
+
+			// End of multipart/form-data.
+			writer.append("--" + boundary + "--").append(CRLF).flush();
+
+			int responseCode = connection.getResponseCode();
+
+			StringBuilder sb = new StringBuilder();
+			BufferedReader br = null;
+			if (responseCode >= 200 && responseCode <= 299) {
+				br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			} else {
+				br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+			}
+
+			String strCurrentLine;
+			while ((strCurrentLine = br.readLine()) != null) {
+				sb.append(strCurrentLine);
+			}
+
+			return Json2Obj.deserializeObj(sb.toString(), AnalyserResponseDTO.class);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		} finally {
+			if (connection != null) {
+				connection.disconnect();
+			}
+		}
+
+		return new AnalyserResponseDTO();
+	}
+
+	public List<InconsistencyErrorDTO> getInconsistenciesByClientId(String clientId) {
+		List<InconsistencyErrorDTO> response = new ArrayList<>();
+		HttpURLConnection connection = null;
+
+		try {
+//			System.out.println("------------------------ GET ------------------------");
+
+			String urlWithClient = this.urlBase + "/inconsistencies/" + clientId;
+			URL url = new URL(urlWithClient);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setConnectTimeout(5000);
+			connection.setReadTimeout(5000);
+
+			int responseCode = connection.getResponseCode();
+
+			StringBuilder sb = new StringBuilder();
+			BufferedReader br = null;
+			if (responseCode >= 200 && responseCode <= 299) {
+				br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			} else {
+				br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+			}
+
+			String strCurrentLine;
+			while ((strCurrentLine = br.readLine()) != null) {
+				sb.append(strCurrentLine);
+			}
+
+//			System.out.println("Response Code: " + responseCode);
+//			System.out.println("Response Body: " + sb.toString());
+
+			return Json2Obj.deserializeCollection(sb.toString(), InconsistencyErrorDTO.class);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		} finally {
+			if (connection != null) {
+				connection.disconnect();
+			}
+		}
+
+		return response;
+	}
+}
